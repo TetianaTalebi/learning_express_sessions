158bd4e63263666d3250b0dab487d49cb5e73fb7
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-02 23:43:42 -0400

    Create an object with the secret string for session in index.js for demo purposes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const session = require('express-session');
+
 const app = express();
+
+// Create an object with the secret string for session
+// We keep the secret string in index.js only for demo purposes!!!
+app.use(session({secret: 'notagoodsecret'}));
 
 
 app.listen(3000, ()=>{
